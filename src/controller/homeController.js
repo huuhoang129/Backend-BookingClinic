ed6b8c5e5d826545629be56775ad4f353ee785b5
Le Ngoc Hoang <dinoradio12z@gmail.com>
2025-03-18 13:42:57 +0700

    update project

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,11 +1,9 @@
-import db from "../models/index"
+import db from "../models/index";
+import CRUDService from "../services/CRUDService";
 
 let getHomePage = async (req, res) => {
     try {
         let data = await db.User.findAll();
-        console.log('--------------------------')
-        console.log(data)
-        console.log('--------------------------')
         return res.render('homepage.ejs', {
             data: JSON.stringify(data)
         });
@@ -14,6 +12,34 @@ let getHomePage = async (req, res) => {
     }
 }
 
+let getAboutPage = (req, res) => {
+    return res.render('test/about.ejs')
+}
+
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs')
+}
+
+let postCRUD = async (req, res) => {
+    let message = await CRUDService.createNewUser(req.body)
+    console.log(message)
+    return res.send('post crud')
+}
+
+let displayGetCRUD = async (req, res) => {
+    let data = await CRUDService.getAllUser();
+    console.log('----------------')
+    console.log(data)
+    console.log('----------------')
+    return res.render('displayCRUD.ejs', {
+        dataTable: data
+    });
+}
+
 module.exports = {
     getHomePage: getHomePage,
+    getAboutPage: getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
+    displayGetCRUD: displayGetCRUD,
 }
