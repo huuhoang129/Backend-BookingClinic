dfc0ce5e81b25c10da69f0ac16a556fcb6c8e11e
Le Ngoc Hoang <dinoradio12z@gmail.com>
2025-04-23 17:51:45 +0700

    Repair Database and Create API Top Doctor

--- src/route/web.js
+++ src/route/web.js
@@ -1,6 +1,7 @@
 import express from "express";
 import homeController from "../controller/homeController";
 import userController from "../controller/userController";
+import doctorController from "../controller/doctorController";
 
 let router = express.Router();
 
@@ -23,6 +24,8 @@ let initWebRoutes = (app) => {
     router.delete('/api/delete-user', userController.handleDeleteUser);
 
     router.get('/api/allcode', userController.getAllCode);
+    router.get('/api/top-doctor-home', doctorController.getTopDoctorHome);
+
     return app.use("/", router);
 }
 
