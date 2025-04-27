e672a68d5d154c5c7a84b570a818392860d9b492
Le Ngoc Hoang <dinoradio12z@gmail.com>
2025-04-27 15:29:32 +0700

    Fix and Updaye Data Schedule

--- src/services/doctorServices.js
+++ src/services/doctorServices.js
@@ -168,16 +168,13 @@ let bulkCreateSchedule = (data) => {
                         raw: true
                     }
                 );
-                if (existing && existing.length > 0) {
-                    existing = existing.map(item => {
-                        item.date = new Date(item.date).getTime();
-                        return item
-                    })
-                }
+
+                //compare diffrent
                 let toCreate = _.differenceWith(schedule, existing, (a, b) => {
-                    return a.timeType === b.timeType && a.date === b.date
+                    return a.timeType === b.timeType && +a.date === +b.date
                 });
 
+                //create data
                 if (toCreate && toCreate.length > 0) {
                     await db.Schedule.bulkCreate(toCreate)
                 }
@@ -210,6 +207,11 @@ let getScheduleByDate = (doctorId, date) => {
                         doctorId: doctorId,
                         date: date
                     },
+                    include: [
+                        { model: db.Allcode, as: 'timeTypeData', attributes: ['valueEn', 'valueVi'] }
+                    ],
+                    raw: false,
+                    nest: true
                 })
 
                 if (!dataSchedule) dataSchedule = [];
