011c66edfc86b01a894d6c3a699a2e446af7cb50
Le Ngoc Hoang <dinoradio12z@gmail.com>
2025-05-01 01:11:06 +0700

    Write API Get All Clinics

--- src/services/clinicService.js
+++ src/services/clinicService.js
@@ -32,7 +32,66 @@ let createClinic = (data) => {
         }
     })
 }
+let getAllClinic = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let data = await db.Clinic.findAll({
+
+            });
+            if (data && data.length > 0) {
+                data.map(item => {
+                    item.image = new Buffer(item.image, 'base64').toString('binary');
+                    return item;
+                })
+            }
+            resolve({
+                errCode: 0,
+                errMessage: 'OK',
+                data
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+let getDetailClinicById = (inputId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!inputId) {
+                // if (!data.email) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing parameter'
+                })
+            } else {
+                let data = await db.Clinic.findOne({
+                    where: {
+                        id: inputId
+                    },
+                    attributes: ['name', 'address', 'descriptionHTML', 'descriptionMarkdown'],
+                })
+                if (data) {
+                    let doctorClinic = [];
+                    doctorClinic = await db.Doctor_Infor.findAll({
+                        where: { clinicId: inputId },
+                        attributes: ['doctorId', 'provinceId'],
+                    })
+                    data.doctorClinic = doctorClinic;
+                } else data = {}
+                resolve({
+                    errMessage: 'OK',
+                    errCode: 0,
+                    data
+                })
+            }
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
 
 module.exports = {
     createClinic: createClinic,
+    getAllClinic: getAllClinic,
+    getDetailClinicById: getDetailClinicById
 }
